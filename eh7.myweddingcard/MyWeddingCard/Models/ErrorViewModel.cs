abc7db737cd23ad81eacaa43f94087caa1e65af4
H-EH7 <fly3767@gmail.com>
2023-12-23 12:36:31 +0900

    init

---
+++ eh7.myweddingcard/MyWeddingCard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWeddingCard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
